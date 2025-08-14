16325b69fa5804b8e21a19824c4178258d758101
papanda <ru3aah@gmail.com>
2025-08-14 15:14:11 +0300

    update users.models

--- users/models.py
+++ users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
+class User(AbstractUser):
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username", "first_name", "last_name"]
+
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.email
