73759ca546948dec8a340c5022beb42424f90bba
papanda <ru3aah@gmail.com>
2025-08-21 18:20:39 +0300

    updated urls.py

--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from config.settings import DEBUG, MEDIA_URL, MEDIA_ROOT
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -10,5 +11,7 @@ urlpatterns = [
 ]
 
 # Serve media files during development
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if DEBUG:
+    import debug_toolbar
+
+    urlpatterns += static(MEDIA_URL, document_root=MEDIA_ROOT)
