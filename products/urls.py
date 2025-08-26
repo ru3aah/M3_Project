4080caf811b51d371532cd39a56af0dff5057efe
papanda <ru3aah@gmail.com>
2025-08-26 13:29:16 +0300

    add doc string to Product:views

--- products/urls.py
+++ products/urls.py
@@ -7,5 +7,6 @@ app_name = "products"
 urlpatterns = [
     path("home/", ProductListView.as_view(), name="product-list"),
     path("guides/", GuidesRecipesView.as_view(), name="guides-recipes"),
-    path("<slug:slug>/", ProductDetailView.as_view(), name="product-detail"),
+    path("<slug:slug>/", ProductDetailView.as_view(),
+                 name="product-detail"),
 ]
